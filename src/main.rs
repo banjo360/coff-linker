25279e97d5be9765f285e93c32d3f63fb114f79d
minirop <minirop@hotmail.com>
2024-04-23 20:39:43 +0200

    .rdata sections are now matched with their names allowing for automatic linking of strings/floats.

--- src/main.rs
+++ src/main.rs
@@ -67,7 +67,7 @@ fn main() -> Result<()> {
     assert_eq!(characteristics, 0x0180);
 
     let mut symbols: Vec<String> = vec![String::new(); symbols_count as usize];
-    let mut text_symbols = HashMap::<u16, String>::new();
+    let mut section_symbol_names = HashMap::<u16, Vec<String>>::new();
 
     let pos = f.stream_position()?;
     f.seek(SeekFrom::Start(symbols_table as u64))?;
@@ -99,10 +99,10 @@ fn main() -> Result<()> {
         let _storage_class = f.read_u8()?;
         let number_of_aux_symbols = f.read_u8()?;
 
-        if name == ".text" {
-            text_symbols.insert(section_number, "".to_string());
-        } else if text_symbols.contains_key(&section_number) && text_symbols[&section_number].len() == 0 {
-            text_symbols.insert(section_number, name);
+        if section_symbol_names.contains_key(&section_number) {
+            section_symbol_names.get_mut(&section_number).unwrap().push(name);
+        } else {
+            section_symbol_names.insert(section_number, vec![name]);
         }
 
         let mut aux = vec![0; (number_of_aux_symbols * 18) as usize];
@@ -110,7 +110,6 @@ fn main() -> Result<()> {
 
         id += 1u32 + (number_of_aux_symbols as u32);
     }
-    f.seek(SeekFrom::Start(pos))?;
 
     let path = Path::new(&args.filename);
     let path = path.file_stem().unwrap();
@@ -123,135 +122,156 @@ fn main() -> Result<()> {
     let _ = fs::remove_file(format!("{}{}.data.bin", output, path));
     let _ = fs::remove_file(format!("{}{}.rdata.bin", output, path));
 
-    f.seek(SeekFrom::Start(pos))?;
-    for section_id in 1..=sections_count {
-        let mut buff = vec![0; 8usize];
-        f.read(&mut buff).unwrap();
-        let name = String::from_utf8(buff).unwrap().trim_matches(char::from(0)).to_string();
-        let _size_mem = f.read_u32::<LittleEndian>()?;
-        let _virt_addr = f.read_u32::<LittleEndian>()?;
-        let size_raw = f.read_u32::<LittleEndian>()?;
-        let raw_data = f.read_u32::<LittleEndian>()?;
-        let reloc_ptr = f.read_u32::<LittleEndian>()?;
-        let _lines_ptr = f.read_u32::<LittleEndian>()?;
-        let reloc_count = f.read_u16::<LittleEndian>()?;
-        let _lines_count = f.read_u16::<LittleEndian>()?;
-        let _characs = f.read_u32::<LittleEndian>()?;
+    let mut extra_data_offset = 0;
 
-        if name == ".text" {
-            let pos = f.stream_position()?;
-            let self_name = &text_symbols[&section_id];
-            let self_addr = symbol_addresses[self_name];
+    for i in 0..2 {
+        let parse_text = i == 1;
 
-            f.seek(SeekFrom::Start(raw_data as u64))?;
-            let mut buff = vec![0; size_raw as usize];
+        f.seek(SeekFrom::Start(pos))?;
+        for section_id in 1..=sections_count {
+            let mut buff = vec![0; 8usize];
             f.read(&mut buff).unwrap();
-            if reloc_count > 0 {
-                f.seek(SeekFrom::Start(reloc_ptr as u64))?;
-                let mut extra_data_offset = buff.len() as u64;
-                for _ in 0..reloc_count {
-                    let offset = f.read_u32::<LittleEndian>()?;
-                    let symtab_index = f.read_u32::<LittleEndian>()?;
-                    let type_ = f.read_u16::<LittleEndian>()?;
+            let name = String::from_utf8(buff).unwrap().trim_matches(char::from(0)).to_string();
+            let _size_mem = f.read_u32::<LittleEndian>()?;
+            let _virt_addr = f.read_u32::<LittleEndian>()?;
+            let size_raw = f.read_u32::<LittleEndian>()?;
+            let raw_data = f.read_u32::<LittleEndian>()?;
+            let reloc_ptr = f.read_u32::<LittleEndian>()?;
+            let _lines_ptr = f.read_u32::<LittleEndian>()?;
+            let reloc_count = f.read_u16::<LittleEndian>()?;
+            let _lines_count = f.read_u16::<LittleEndian>()?;
+            let _characs = f.read_u32::<LittleEndian>()?;
 
-                    if type_ == 18 {
-                        // skip IMAGE_REL_PPC_PAIR
-                        continue;
+            if name == ".text" && !parse_text {
+                let mut text_section_addr = 0;
+                for (_, v) in section_symbol_names.iter() {
+                    if v[0] == ".text" {
+                        text_section_addr = symbol_addresses[&v[1]];
                     }
+                }
 
-                    let sym_name = &symbols[symtab_index as usize];
-                    let patched = if symbol_addresses.contains_key(sym_name) {
-                        symbol_addresses[sym_name] as u32
-                    } else {
-                        let new_addr = self_addr + extra_data_offset;
-                        symbol_addresses.insert(sym_name.clone(), new_addr);
-                        extra_data_offset += 0;
-                        new_addr as u32
-                    };
+                extra_data_offset = text_section_addr as u32 + size_raw;
+            }
 
-                    let instruction = buff[offset as usize] & 0b11111100;
+            if name == ".text" && parse_text {
+                let pos = f.stream_position()?;
+                let self_name_vec = &section_symbol_names[&section_id];
+                assert_eq!(name, self_name_vec[0]);
+                let self_name = &self_name_vec[1];
 
-                    if instruction == B_INST {
-                        assert_eq!(type_, 0x0006);
+                f.seek(SeekFrom::Start(raw_data as u64))?;
+                let mut buff = vec![0; size_raw as usize];
+                f.read(&mut buff).unwrap();
+                if reloc_count > 0 {
+                    f.seek(SeekFrom::Start(reloc_ptr as u64))?;
+                    for _ in 0..reloc_count {
+                        let offset = f.read_u32::<LittleEndian>()?;
+                        let symtab_index = f.read_u32::<LittleEndian>()?;
+                        let type_ = f.read_u16::<LittleEndian>()?;
 
-                        if !symbol_addresses.contains_key(self_name) {
-                            panic!("Can't find address of {self_name}");
+                        if type_ == 18 {
+                            // skip IMAGE_REL_PPC_PAIR
+                            continue;
                         }
 
-                        let curr_addr = symbol_addresses[self_name] as u32 + offset;
-                        let addr_diff = ((patched as i64) - (curr_addr as i64)) / 4;
-     
-                        buff[offset as usize]       = (buff[offset as usize] & 0b11111100) | ((addr_diff >> 22) & 0b11) as u8;
-                        buff[(offset + 1) as usize] = ((addr_diff >> 14) & 0xff) as u8;
-                        buff[(offset + 2) as usize] = ((addr_diff >> 6) & 0xff) as u8;
-                        buff[(offset + 3) as usize] = (buff[(offset + 3) as usize] & 0b11) | ((addr_diff << 2) & 0b11111100) as u8;
-                    } else if instruction == ADDIS_INST {
-                        assert_eq!(type_, 0x0010);
-                        let third_byte = if (patched & 0x8000) != 0 {
-                            (((patched >> 16) + 1) & 0xff) as u8
+                        let sym_name = &symbols[symtab_index as usize];
+                        let patched = if symbol_addresses.contains_key(sym_name) {
+                            symbol_addresses[sym_name] as u32
                         } else {
-                            ((patched >> 16) & 0xff) as u8
+                            panic!("Unknown symbol: {sym_name}");
                         };
-                        buff[(offset + 2) as usize] = (patched >> 24) as u8;
-                        buff[(offset + 3) as usize] = third_byte;
-                    } else if instruction == LFD_INST {
-                        assert_eq!(type_, 0x0011);
-                        buff[(offset + 2) as usize] = ((patched >> 8) & 0xff) as u8;
-                        buff[(offset + 3) as usize] = (patched & 0xff) as u8;
-                    } else if instruction == LFS_INST {
-                        assert_eq!(type_, 0x0011);
-                        buff[(offset + 2) as usize] = ((patched >> 8) & 0xff) as u8;
-                        buff[(offset + 3) as usize] = (patched & 0xff) as u8;
-                    } else if instruction == ADDI_INST {
-                        assert_eq!(type_, 0x0011);
-                        let third_byte = if (patched & 0x8000) != 0 {
-                            (((patched >> 16) + 1) & 0xff) as u8
+
+                        let instruction = buff[offset as usize] & 0b11111100;
+
+                        if instruction == B_INST {
+                            assert_eq!(type_, 0x0006);
+
+                            if !symbol_addresses.contains_key(self_name) {
+                                panic!("Can't find address of '{self_name}'");
+                            }
+
+                            let curr_addr = symbol_addresses[self_name] as u32 + offset;
+                            let addr_diff = ((patched as i64) - (curr_addr as i64)) / 4;
+         
+                            buff[offset as usize]       = (buff[offset as usize] & 0b11111100) | ((addr_diff >> 22) & 0b11) as u8;
+                            buff[(offset + 1) as usize] = ((addr_diff >> 14) & 0xff) as u8;
+                            buff[(offset + 2) as usize] = ((addr_diff >> 6) & 0xff) as u8;
+                            buff[(offset + 3) as usize] = (buff[(offset + 3) as usize] & 0b11) | ((addr_diff << 2) & 0b11111100) as u8;
+                        } else if instruction == ADDIS_INST {
+                            assert_eq!(type_, 0x0010);
+                            let third_byte = if (patched & 0x8000) != 0 {
+                                (((patched >> 16) + 1) & 0xff) as u8
+                            } else {
+                                ((patched >> 16) & 0xff) as u8
+                            };
+                            buff[(offset + 2) as usize] = (patched >> 24) as u8;
+                            buff[(offset + 3) as usize] = third_byte;
+                        } else if instruction == LFD_INST {
+                            assert_eq!(type_, 0x0011);
+                            buff[(offset + 2) as usize] = ((patched >> 8) & 0xff) as u8;
+                            buff[(offset + 3) as usize] = (patched & 0xff) as u8;
+                        } else if instruction == LFS_INST {
+                            assert_eq!(type_, 0x0011);
+                            buff[(offset + 2) as usize] = ((patched >> 8) & 0xff) as u8;
+                            buff[(offset + 3) as usize] = (patched & 0xff) as u8;
+                        } else if instruction == ADDI_INST {
+                            assert_eq!(type_, 0x0011);
+                            let third_byte = if (patched & 0x8000) != 0 {
+                                (((patched >> 16) + 1) & 0xff) as u8
+                            } else {
+                                ((patched >> 16) & 0xff) as u8
+                            };
+                            buff[(offset + 2) as usize] = (patched >> 24) as u8;
+                            buff[(offset + 3) as usize] = third_byte;
                         } else {
-                            ((patched >> 16) & 0xff) as u8
-                        };
-                        buff[(offset + 2) as usize] = (patched >> 24) as u8;
-                        buff[(offset + 3) as usize] = third_byte;
-                    } else {
-                        panic!("{sym_name}: Unknown instruction 0x{:x} ({}) at offset {:#X}", instruction, instruction >> 2, offset + raw_data);
+                            panic!("{sym_name}: Unknown instruction 0x{:x} ({}) at offset {:#X}", instruction, instruction >> 2, offset + raw_data);
+                        }
                     }
                 }
-            }
 
-            std::fs::write(format!("{}{}.bin", output, self_name), buff)?;
-            f.seek(SeekFrom::Start(pos))?;
-        } else if name == ".data" || name == ".rdata" {
-            let pos = f.stream_position()?;
+                std::fs::write(format!("{}{}.bin", output, self_name), buff)?;
+                f.seek(SeekFrom::Start(pos))?;
+            } else if (name == ".data" || name == ".rdata") && !parse_text {
+                let pos = f.stream_position()?;
+                let self_name_vec = &section_symbol_names[&section_id];
+                assert_eq!(name, self_name_vec[0]);
+                let self_name = &self_name_vec[1];
 
-            f.seek(SeekFrom::Start(raw_data as u64))?;
-            let mut buff = vec![0; size_raw as usize];
-            f.read(&mut buff).unwrap();
-            if reloc_count > 0 {
-                f.seek(SeekFrom::Start(reloc_ptr as u64))?;
-                for _ in 0..reloc_count {
-                    let offset = f.read_u32::<LittleEndian>()?;
-                    let symtab_index = f.read_u32::<LittleEndian>()?;
-                    let type_ = f.read_u16::<LittleEndian>()?;
-                    assert_eq!(type_, 0x0002);
+                let new_addr = extra_data_offset;
+                symbol_addresses.insert(self_name.clone(), new_addr.into());
+                extra_data_offset += size_raw;
 
-                    let sym_name = &symbols[symtab_index as usize];
-                    let patched = if symbol_addresses.contains_key(sym_name) {
-                        symbol_addresses[sym_name] as u32
-                    } else {
-                        panic!("symbol '{}' not found.", sym_name);
-                    };
- 
-                    buff[offset as usize]       = (patched >> 24) as u8;
-                    buff[(offset + 1) as usize] = (patched >> 16) as u8;
-                    buff[(offset + 2) as usize] = (patched >> 8)  as u8;
-                    buff[(offset + 3) as usize] = patched         as u8;
+                f.seek(SeekFrom::Start(raw_data as u64))?;
+                let mut buff = vec![0; size_raw as usize];
+                f.read(&mut buff).unwrap();
+                if reloc_count > 0 {
+                    f.seek(SeekFrom::Start(reloc_ptr as u64))?;
+                    for _ in 0..reloc_count {
+                        let offset = f.read_u32::<LittleEndian>()?;
+                        let symtab_index = f.read_u32::<LittleEndian>()?;
+                        let type_ = f.read_u16::<LittleEndian>()?;
+                        assert_eq!(type_, 0x0002);
+
+                        let sym_name = &symbols[symtab_index as usize];
+                        let patched = if symbol_addresses.contains_key(sym_name) {
+                            symbol_addresses[sym_name] as u32
+                        } else {
+                            panic!("symbol '{}' not found.", sym_name);
+                        };
+     
+                        buff[offset as usize]       = (patched >> 24) as u8;
+                        buff[(offset + 1) as usize] = (patched >> 16) as u8;
+                        buff[(offset + 2) as usize] = (patched >> 8)  as u8;
+                        buff[(offset + 3) as usize] = patched         as u8;
+                    }
                 }
-            }
 
-            let mut output_file = File::options().create(true).write(true).append(true).open(format!("{}{}{}.bin", output, path, name))?;
-            output_file.write(&buff)?;
-            f.seek(SeekFrom::Start(pos))?;
+                let mut output_file = File::options().create(true).write(true).append(true).open(format!("{}{}{}.bin", output, path, name))?;
+                output_file.write(&buff)?;
+                f.seek(SeekFrom::Start(pos))?;
+            }
         }
     }
-    
+
     Ok(())
 }
